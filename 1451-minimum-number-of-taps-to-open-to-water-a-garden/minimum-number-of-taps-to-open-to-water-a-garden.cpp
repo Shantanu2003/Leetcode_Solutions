66e074ca74eaace4e6d09a5cd58d12d5ddc4ffa4
Shantanu Singh Verma <90418655+Shantanu2003@users.noreply.github.com>
2023-09-01 01:14:31 +0530

    Time: 22 ms (73.08%) | Memory: 14 MB (91.92%) - LeetSync

--- 1451-minimum-number-of-taps-to-open-to-water-a-garden/minimum-number-of-taps-to-open-to-water-a-garden.cpp
+++ 1451-minimum-number-of-taps-to-open-to-water-a-garden/minimum-number-of-taps-to-open-to-water-a-garden.cpp
@@ -3,12 +3,12 @@ public:
     int minTaps(int n, vector<int>& ranges) {
         int min= 0; 
         int max= 0; int open = 0;
-        //int index= 0;
+        int index= 0;
         while(max<n){
-             for(int i = 0; i<ranges.size() ; i++){
+             for(int i = index; i<ranges.size() ; i++){
                  if(i-ranges[i]<=min && i+ ranges[i]>= max){
                      max= i+ ranges[i];
-                     //index= i;
+                     index= i;
                  }
              }
                  if(min == max)return -1;
