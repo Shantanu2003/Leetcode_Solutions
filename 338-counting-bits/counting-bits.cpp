1f82554eefcc0716931399760f09a7a14343d272
Shantanu Singh Verma <90418655+Shantanu2003@users.noreply.github.com>
2023-09-02 02:10:21 +0530

    Time: 3 ms (89.66%) | Memory: 8 MB (51.09%) - LeetSync

--- 338-counting-bits/counting-bits.cpp
+++ 338-counting-bits/counting-bits.cpp
@@ -1,16 +1,38 @@
 class Solution {
 public:
     vector <int> countBits(int num) {
-        vector<int> res(num);
-        res.push_back(0);  // for num = 0
-        if (num == 0) return res;
-        
-        for (int i = 1; i <= num; i++) {
-            if (i % 2 == 0) 
-                res[i] = res[i / 2];
-            else
-                res[i] = res[i - 1] + 1;
+        vector<int> ans(num+1);
+        int x = 0;
+        int b = 1;
+        while(b<=num){
+            while(x<b && x+b <=num){
+                ans[x+b] = ans[x] +1;
+                ++x;
+            }
+            x= 0;
+            b<<= 1;
         }
-        return res;
+        return ans;
     }
 };
+
+/* public class Solution {
+    public int[] countBits(int n) {
+        int[] ans = new int[n + 1];
+        int x = 0;
+        int b = 1;
+    
+        // [0, b) is calculated
+        while (b <= n) {
+            // generate [b, 2b) or [b, n) from [0, b)
+            while (x < b && x + b <= n) {
+                ans[x + b] = ans[x] + 1;
+                ++x;
+            }                         
+            x = 0; // reset x
+            b <<= 1; // b = 2b
+        }
+                  
+        return ans;
+    }
+} */
