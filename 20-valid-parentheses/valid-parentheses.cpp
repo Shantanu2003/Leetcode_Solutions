eed440321145f798e18921e5125a06bf45d63dd9
Shantanu Singh Verma <90418655+Shantanu2003@users.noreply.github.com>
2023-09-05 23:46:10 +0530

    Time: 0 ms (100.00%) | Memory: 6.4 MB (11.41%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -1,17 +1,13 @@
-#include <bits/stdc++.h>
 class Solution {
 public:
     bool isValid(string s) {
             stack<char> st;
-            char c;
             bool flag =true;
-            //if(s.size() == 1)flag = false;
 
             for(int i =0; i<s.size(); i++){
    
                 if(s[i] == '{' || s[i] == '(' || s[i] == '['){
-                    c=s[i];
-                    st.push(c);
+                    st.push(s[i]);
                 }
                 else{
 
