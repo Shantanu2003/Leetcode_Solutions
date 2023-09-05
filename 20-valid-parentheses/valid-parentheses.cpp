1bcddf5dabaadbc9751a952b5ef96ef4d4efba68
Shantanu Singh Verma <90418655+Shantanu2003@users.noreply.github.com>
2023-09-05 23:43:58 +0530

    Time: 0 ms (100.00%) | Memory: 6.4 MB (40.26%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -1,16 +1,37 @@
+#include <bits/stdc++.h>
 class Solution {
 public:
     bool isValid(string s) {
-        stack<char> st;  //taking stack for keep tracking the order of the brackets..
-        for(auto i:s)  //iterate over each and every elements
-        {
-            if(i=='(' or i=='{' or i=='[') st.push(i);  //if current element of the string will be opening bracket then we will just simply push it into the stack
-            else  //if control comes to else part, it means that current element is a closing bracket, so check two conditions  current element matches with top of the stack and the stack must not be empty...
-            {
-                if(st.empty() or (st.top()=='(' and i!=')') or (st.top()=='{' and i!='}') or (st.top()=='[' and i!=']')) return false;
-                st.pop();  //if control reaches to that line, it means we have got the right pair of brackets, so just pop it.
-            }
-        }
-        return st.empty();  //at last, it may possible that we left something into the stack unpair so return checking stack is empty or not..
+            stack<char> st;
+            char c;
+            bool flag =true;
+            //if(s.size() == 1)flag = false;
+
+            for(int i =0; i<s.size(); i++){
+   
+                if(s[i] == '{' || s[i] == '(' || s[i] == '['){
+                    c=s[i];
+                    st.push(c);
+                }
+                else{
+                   if(st.empty())
+                   return false;
+
+                   else if(st.top()== '(' && s[i] != ')') 
+                   return false;
+
+                   else if (st.top()== '{' && s[i] != '}')
+                   return false;
+
+                   else if(st.top()== '[' && s[i] != ']')
+                   return false;
+
+
+                   else
+                   st.pop();
+                }
+
+            }     
+    return st.empty();
     }
 };
