caab19310b88d17f45cc854df026a8dc61118f5c
Shantanu Singh Verma <90418655+Shantanu2003@users.noreply.github.com>
2023-09-08 00:16:08 +0530

    Time: 19 ms (10.11%), Space: 9.6 MB (12.31%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -0,0 +1,36 @@
+#include <unordered_map>
+
+class Solution {
+public:
+    string intToRoman(int num) {
+        string ans= "";
+        vector<pair<int, string>> mp = {{1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"}, {100, "C"}, {90, "XC"}, {50, "L"}, {40, "XL"}, {10, "X"}, {9, "IX"}, {5, "V"}, {4, "IV"}, {1, "I"}};
+
+        
+        for(int  i =0 ; i<mp.size();i++){
+            while(mp[i].first <= num){
+                num -= mp[i].first;
+                ans+= mp[i].second;
+            }
+        }
+    return ans;
+    }
+};
+
+/*class Solution {
+    private static final int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};    
+    private static final String[] symbols = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+
+    public String intToRoman(int num) {
+        StringBuilder sb = new StringBuilder();
+        // Loop through each symbol, stopping if num becomes 0.
+        for (int i = 0; i < values.length && num > 0; i++) {
+            // Repeat while the current symbol still fits into num.
+            while (values[i] <= num) {
+                num -= values[i];
+                sb.append(symbols[i]);
+            }
+        }
+        return sb.toString();
+    }
+}*/
