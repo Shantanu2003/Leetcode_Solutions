80e1f03a4c75785ea0a48837a5809e72d5e0b503
Shantanu Singh Verma <90418655+Shantanu2003@users.noreply.github.com>
2023-10-13 14:23:46 +0530

    Time: 9 ms (19.87%), Space: 14 MB (37.3%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.cpp
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.cpp
@@ -1,10 +1,11 @@
 class Solution {
 public:
     int minCostClimbingStairs(vector<int>& cost) {
-        for(int j = cost.size()-3 ;j >=0; j--){
-            cost[j] = cost[j]+min(cost[j+1],cost[j+2]) ;
+        int n = cost.size();
+       
+        for(int  i = n-3 ; i>=0; i--){
+            cost[i] = cost[i] + min(cost[i+1], cost[i+2]);
         }
     return min(cost[0],cost[1]);
-
     }
 };
