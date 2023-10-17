e34ca10248f2eb819f2922797c3ffe112c06ed76
Shantanu Singh Verma <90418655+Shantanu2003@users.noreply.github.com>
2023-10-17 21:15:47 +0530

    Time: 64 ms (31.46%), Space: 43.8 MB (23.25%) - LeetHub

--- 1275-validate-binary-tree-nodes/1275-validate-binary-tree-nodes.cpp
+++ 1275-validate-binary-tree-nodes/1275-validate-binary-tree-nodes.cpp
@@ -1,46 +1,47 @@
 class Solution {
-private:
-int findRoot(int n, vector<int>& left, vector<int>& right) {
-    unordered_set<int> children;
-    children.insert(left.begin(), left.end());
-    children.insert(right.begin(), right.end());
-    
-    for (int i = 0; i < n; i++) {
-        if (children.find(i) == children.end()) {
-            return i;
-        }
-    }
-    
-    return -1;
-}
 public:
     bool validateBinaryTreeNodes(int n, vector<int>& leftChild, vector<int>& rightChild) {
-       int res = findRoot(n,leftChild, rightChild);
-
-       if(res == -1)
-       return false;
-
-       stack<int>st;
-       unordered_set<int>seen;
-       seen.insert(res);
-       st.push(res);
-       
-       while(!st.empty()){
-           int node = st.top();
-           st.pop();
-
-           vector<int>child = {leftChild[node], rightChild[node]};
-
-           for(auto c:child){
-               if(c != -1){
-                   if(seen.find(c)!=seen.end()){
-                       return false;
-                   }
-                   st.push(c);
-                   seen.insert(c);
-               }
-           }
-       }
-       return seen.size() == n;
+        int root = findRoot(n, leftChild, rightChild);
+        if (root == -1) {
+            return false;
+        }
+        
+        unordered_set<int> seen;
+        queue<int> queue;
+        seen.insert(root);
+        queue.push(root);
+        
+        while (!queue.empty()) {
+            int node = queue.front();
+            queue.pop();
+            
+            int children[] = {leftChild[node], rightChild[node]};
+            for (int child : children) {
+                if (child != -1) {
+                    if (seen.find(child) != seen.end()) {
+                        return false;
+                    }
+                    
+                    queue.push(child);
+                    seen.insert(child);
+                }
+            }
+        }
+        
+        return seen.size() == n;
+    }
+    
+    int findRoot(int n, vector<int>& left, vector<int>& right) {
+        unordered_set<int> children;
+        children.insert(left.begin(), left.end());
+        children.insert(right.begin(), right.end());
+        
+        for (int i = 0; i < n; i++) {
+            if (children.find(i) == children.end()) {
+                return i;
+            }
+        }
+        
+        return -1;
     }
 };
