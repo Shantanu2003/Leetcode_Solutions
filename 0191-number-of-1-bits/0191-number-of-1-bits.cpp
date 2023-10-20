2896b9684edd6f2bcdccb9d43f01b3ab0cbb256b
Shantanu Singh Verma <90418655+Shantanu2003@users.noreply.github.com>
2023-10-20 20:46:34 +0530

    Time: 2 ms (37.05%), Space: 6.2 MB (50.13%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     int hammingWeight(uint32_t n) {
-        std::string str = std::bitset<32>(n).to_string();
-        int count = 0;
-        for (int i = 0; i < str.size(); i++) {
-            if (str[i] == '1') { // Compare to the character '1'
-                count++;
-            }
-        }
-        return count;
+    int count = 0;
+    
+    while (n) {
+        n &= (n - 1);
+        count++;
     }
+    
+    return count;
+}
 };
