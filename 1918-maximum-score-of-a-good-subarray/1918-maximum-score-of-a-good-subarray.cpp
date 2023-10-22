fc10d1f41988ba4c5452b52e748d67883c21327d
Shantanu Singh Verma <90418655+Shantanu2003@users.noreply.github.com>
2023-10-22 19:41:26 +0530

    Time: 214 ms (33.99%), Space: 105.7 MB (10.96%) - LeetHub

---
+++ 1918-maximum-score-of-a-good-subarray/1918-maximum-score-of-a-good-subarray.cpp
@@ -0,0 +1,32 @@
+class Solution {
+private:
+    int solve(vector<int>& nums, int k){
+      vector<int> left(k,0);
+      int currMin = INT_MAX;
+      for(int  i = k-1; i>=0; i--){
+          currMin = min(currMin, nums[i]);
+          left[i] = currMin;
+      }
+      
+      vector<int> right;
+      currMin = INT_MAX;
+      for(int i =k ; i<nums.size(); i++){
+          currMin = min(currMin,nums[i]);
+          right.push_back(currMin);
+      }
+      int res= 0;
+      for(int  j = 0 ; j<right.size(); j++){
+          currMin = right[j];
+          int i = lower_bound(left.begin(), left.end(), currMin) - left.begin();
+          int size = (k + j) - i + 1;
+          res = max(res, currMin * size);
+      }
+      return res;
+    }
+public:
+    int maximumScore(vector<int>& nums, int k) {
+       int ans = solve(nums,k);
+       reverse(nums.begin(),nums.end());
+       return max(ans, solve(nums,nums.size()-k-1));
+    }
+};
