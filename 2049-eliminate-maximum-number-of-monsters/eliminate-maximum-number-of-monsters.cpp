e2323d705536af9e3e41043e3dbfde21787d175d
Shantanu Singh Verma <90418655+Shantanu2003@users.noreply.github.com>
2023-11-07 22:32:44 +0530

    Time: 75 ms (99.73%) | Memory: 81.2 MB (97.87%) - LeetSync

--- 2049-eliminate-maximum-number-of-monsters/eliminate-maximum-number-of-monsters.cpp
+++ 2049-eliminate-maximum-number-of-monsters/eliminate-maximum-number-of-monsters.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
-    int eliminateMaximum(vector<int>& dist, vector<int>& speed) {
-        priority_queue<float, vector<float>, greater<float>> heap;
-        
-        for (int i = 0; i < dist.size(); i++) {
-            heap.push((float) dist[i] / speed[i]);
+    int eliminateMaximum(std::vector<int>& dist, std::vector<int>& speed) {
+        int n = dist.size();
+        for (int i = 0; i < n; ++i) {
+            dist[i] = std::ceil(static_cast<double>(dist[i]) / speed[i]);
+            speed[i] = 0;
         }
-
-        int ans = 0;
-        while (!heap.empty()) {
-            if (heap.top() <= ans) {
-                break;
+        for (int num : dist) {
+            if (num >= n) continue;
+            speed[num] += 1;
+        }
+        for (int i = 1; i < n; ++i) {
+            speed[i] += speed[i - 1];
+            if (speed[i] > i) {
+                return i;
             }
-            
-            ans++;
-            heap.pop();
         }
-        
-        return ans;
+        return n;
     }
 };
