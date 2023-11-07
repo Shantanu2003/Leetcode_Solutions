5293f10f8f0634bb319a697bde844f0b13cf57c7
Shantanu Singh Verma <90418655+Shantanu2003@users.noreply.github.com>
2023-11-08 00:26:03 +0530

    Time: 129 ms (29.64%) | Memory: 57.8 MB (44.80%) - LeetSync

--- 969-number-of-recent-calls/number-of-recent-calls.cpp
+++ 969-number-of-recent-calls/number-of-recent-calls.cpp
@@ -1,28 +1,17 @@
 class RecentCounter {
 public:
     queue<int> q;
-    int count;
-    queue<int> tempq;
+  
     RecentCounter() {
-        count = 0;
+        
     }
     
     int ping(int t) {
-        count=0; 
         q.push(t);
-        while(!q.empty()){
-            if(q.front()>= (t-3000)){
-                tempq.push(q.front());
-                count++;
-            }
-
+        while (!q.empty() && q.front() < (t - 3000)) {
             q.pop();
         }
-        while (!tempq.empty()) {
-            q.push(tempq.front());
-            tempq.pop();
-        }
-        return count;
+        return q.size();
     }
 };
 
