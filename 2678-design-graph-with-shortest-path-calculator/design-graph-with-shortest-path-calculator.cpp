b6c51d1de80f23fb146a8fa541f383b200cff70e
Shantanu Singh Verma <90418655+Shantanu2003@users.noreply.github.com>
2023-11-11 19:58:17 +0530

    Time: 264 ms (57.70%) | Memory: 88 MB (31.93%) - LeetSync

--- 2678-design-graph-with-shortest-path-calculator/design-graph-with-shortest-path-calculator.cpp
+++ 2678-design-graph-with-shortest-path-calculator/design-graph-with-shortest-path-calculator.cpp
@@ -1,45 +1,68 @@
 class Graph {
 public:
-    vector<vector<pair<int, int>>> adjList;
+    unordered_map<int, vector<pair<int, int>>> graph;
+    priority_queue <int, vector<int>, greater<int>>q;
+
     Graph(int n, vector<vector<int>>& edges) {
-        adjList.resize(n);
-        for (auto& e: edges)
-            adjList[e[0]].push_back(make_pair(e[1], e[2]));
+        for(int  i =0 ; i<edges.size() ; i++){
+            int from = edges[i][0];
+            int to = edges[i][1];
+            int weight = edges[i][2];
+            graph[from].push_back({to, weight});
+            graph[to]; 
+        }
     }
-
+    
     void addEdge(vector<int> edge) {
-        adjList[edge[0]].push_back(make_pair(edge[1], edge[2]));
-    }
+        int from = edge[0];
+        int to = edge[1];
+        int weight= edge[2];
 
+        graph[from].push_back({to, weight});
+        graph[to]; 
+    }
+    
     int shortestPath(int node1, int node2) {
-        int n = adjList.size();
-        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> pq;
-        vector<int> costForNode(n, INT_MAX);
-        costForNode[node1] = 0;
+        unordered_map<int, int> dist;
+        for (auto& node : graph) {
+            dist[node.first] = INT_MAX;
+        }
+
+        priority_queue<pair<int, int>, vector<pair<int, int>>,greater<pair<int, int>>> pq;
+        dist[node1] = 0;
         pq.push({0, node1});
 
         while (!pq.empty()) {
-            int currCost = pq.top()[0];
-            int currNode = pq.top()[1];
+            int u = pq.top().second;
+            int d = pq.top().first;
             pq.pop();
 
-            if (currCost > costForNode[currNode]) {
-                continue;
-            }
-            if (currNode == node2) {
-                return currCost;
-            }
-            for (auto& neighbor : adjList[currNode]) {
-                int neighborNode = neighbor.first;
-                int cost = neighbor.second;
-                int newCost = currCost + cost;
+            if (u == node2) {
+            return dist[u];
+        }
 
-                if (newCost < costForNode[neighborNode]) {
-                    costForNode[neighborNode] = newCost;
-                    pq.push({newCost, neighborNode});
+            if (d > dist[u]) continue;
+
+            for (auto& edge : graph[u]) {
+                int v = edge.first;
+                int weight = edge.second;
+
+                if (dist[v] > dist[u] + weight) {
+                    dist[v] = dist[u] + weight;
+                    pq.push({dist[v], v});
                 }
             }
         }
-        return -1;
+        if (dist[node2] == INT_MAX) {
+        return -1; // No path found
+            }
+        return dist[node2];
     }
 };
+
+/**
+ * Your Graph object will be instantiated and called as such:
+ * Graph* obj = new Graph(n, edges);
+ * obj->addEdge(edge);
+ * int param_2 = obj->shortestPath(node1,node2);
+ */
