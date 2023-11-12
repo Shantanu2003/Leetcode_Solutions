ffd7099e3a3703ed724627bd14515281a1a65e98
Shantanu Singh Verma <90418655+Shantanu2003@users.noreply.github.com>
2023-11-12 23:32:04 +0530

    Time: 34 ms (45.52%) | Memory: 35.2 MB (21.48%) - LeetSync

--- 783-search-in-a-binary-search-tree/search-in-a-binary-search-tree.cpp
+++ 783-search-in-a-binary-search-tree/search-in-a-binary-search-tree.cpp
@@ -11,24 +11,16 @@
  */
 class Solution {
 public:
-    TreeNode* helper(TreeNode* root,int val){
-        if(root == NULL){
-            return NULL;
-        }
-        if(root->val == val){
-            return root;
-        }
-        else if(root->val > val){
-        return helper(root->left,val);
-        }
-        
-        else if(root->val < val){
-        return helper(root->right,val);
-        }
-
-       return NULL;
-    }
     TreeNode* searchBST(TreeNode* root, int val) {
-        return helper(root,val);
+        while (root != NULL) {
+            if (root->val == val) {
+                return root;
+            } else if (root->val > val) {
+                root = root->left;
+            } else {
+                root = root->right;
+            }
+        }
+        return NULL;
     }
 };
