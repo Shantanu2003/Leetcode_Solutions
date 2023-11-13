a6ca890eb6371224bd2aef38570833608846f9f4
Shantanu Singh Verma <90418655+Shantanu2003@users.noreply.github.com>
2023-11-13 22:29:50 +0530

    Time: 32 ms (10.97%) | Memory: 33.1 MB (33.40%) - LeetSync

---
+++ 450-delete-node-in-a-bst/delete-node-in-a-bst.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        if(root) 
+            if(key < root->val) root->left = deleteNode(root->left, key);     
+            else if(key > root->val) root->right = deleteNode(root->right, key);       
+            else{
+                if(!root->left && !root->right) return NULL;          
+                if (!root->left || !root->right)
+                    return root->left ? root->left : root->right;   
+					                                                  
+                TreeNode* temp = root->right;                        
+                while(temp->left != NULL) temp = temp->left;     
+                root->val = temp->val;                            
+                root->right = deleteNode(root->right, temp->val);  
+            }
+        return root;
+    }   
+};
