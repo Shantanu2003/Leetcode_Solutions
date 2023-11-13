b59f34c62b9266a01ad7ddccc1b1afa6795a8a52
Shantanu Singh Verma <90418655+Shantanu2003@users.noreply.github.com>
2023-11-13 18:02:23 +0530

    Time: 56 ms (21.42%) | Memory: 15 MB (28.66%) - LeetSync

--- 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.cpp
+++ 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.cpp
@@ -1,19 +1,29 @@
 class Solution {
 public:
     string sortVowels(string s) {
-        vector<int> ans;
-        vector<int>index;
-        for(int i = 0; i<s.size() ; i++){
-          if(s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i]== 'u' || s[i] == 'A' || s[i] == 'E' || s[i]  == 'I'|| s[i] == 'O' || s[i] == 'U'){
-             ans.push_back(s[i]);
-             index.push_back(i);
-            }
+    unordered_set<char> vowels = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
+    string vowelChars;
+
+    for (char ch : s) {
+        if (vowels.count(ch)) {
+            vowelChars.push_back(ch);
         }
-        sort(ans.begin(), ans.end());
-       
-        for(int i = 0 ; i < index.size() ; i++){
-            s[index[i]] = ans[i];
+    }
+
+    sort(vowelChars.begin(), vowelChars.end());
+
+    string result;
+    int vowelIndex = 0;
+
+    for (char ch : s) {
+        if (vowels.count(ch)) {
+            result.push_back(vowelChars[vowelIndex]);
+            vowelIndex++;
+        } else {
+            result.push_back(ch);
         }
-        return s;
+    }
+
+    return result;
     }
 };
