abd8d7c99fc3d44e91e702914e8ab14b26f712e2
Shantanu Singh Verma <90418655+Shantanu2003@users.noreply.github.com>
2023-11-16 00:37:46 +0530

    Time: 86 ms (21.64%) | Memory: 51.5 MB (96.27%) - LeetSync

--- 1956-maximum-element-after-decreasing-and-rearranging/maximum-element-after-decreasing-and-rearranging.cpp
+++ 1956-maximum-element-after-decreasing-and-rearranging/maximum-element-after-decreasing-and-rearranging.cpp
@@ -2,50 +2,17 @@ class Solution {
 public:
     int maximumElementAfterDecrementingAndRearranging(vector<int>& arr) {
         sort(arr.begin(), arr.end());
-        int indx = -1;
-        int max_ele = 0;
-        bool flag = false, flag1 = false;
-        if(arr[0] == 1){
-            flag1 = true;
-        }
-        else{
-            arr[0] = 1;
-            flag1 = true;
-        }
+        arr[0] = 1;
 
-        if(flag1 == true){
-            flag = true;
-            for(int i = 1; i < arr.size() ; i++){
-                if(arr[i] - arr[i-1] > 1){
-                    flag = false;
-                    indx = i;
-                    break;
-                }
+        // Iterate through the array to satisfy the conditions
+        for (int i = 1; i < arr.size(); i++) {
+            // Ensure the absolute difference between adjacent elements is less than or equal to 1
+            if (arr[i] - arr[i - 1] > 1) {
+                arr[i] = arr[i - 1] + 1;
             }
         }
-        int max_ele_before_change = arr[arr.size() -1];
 
-        if(arr.size() > max_ele_before_change){
-            if(arr[arr.size()-1] - arr[arr.size() - 2] > 1)
-            max_ele = arr[arr.size() -2] +1;
-
-           else
-           max_ele = max_ele_before_change;
-
-        }
-       
-        else if(flag == true){
-            max_ele = arr[arr.size() - 1];
-        }
-
-        else{
-            arr[indx] = arr[indx -1]+1; 
-            for(int i = indx + 1; i < arr.size() ; i++){
-                arr[i] = arr[i-1] +1;
-            }
-            max_ele = arr[arr.size() - 1];
-
-        }
-        return max_ele;
+        // The maximum element is now at the end of the sorted and updated array
+        return arr[arr.size() -1];
     }
 };
