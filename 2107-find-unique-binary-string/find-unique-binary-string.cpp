43615e1eb7d60606e22fc1d3bcd0576dc93a2265
Shantanu Singh Verma <90418655+Shantanu2003@users.noreply.github.com>
2023-11-16 20:17:40 +0530

    Time: 3 ms (74.62%) | Memory: 10.6 MB (64.35%) - LeetSync

--- 2107-find-unique-binary-string/find-unique-binary-string.cpp
+++ 2107-find-unique-binary-string/find-unique-binary-string.cpp
@@ -1,38 +1,19 @@
 class Solution {
 public:
-
-vector<bitset<32>> helper(int n) {
-    vector<bitset<32>> combinations;
-
-    for (int i = 0; i < (1 << n); ++i) {
-        combinations.push_back(i);
-    }
-
-    return combinations;
-}
-   string findDifferentBinaryString(vector<string>& nums) {
-        unordered_set<string>ans;
-        int n = nums[0].size();
-        vector<bitset<32>> combinations;
-
-
-        for(int  i = 0 ; i  <nums.size() ; i++){
-            ans.insert(nums[i]);
+    string findDifferentBinaryString(vector<string>& nums) {
+        unordered_set<int> integers;
+        for (string num : nums) {
+            integers.insert(stoi(num, 0, 2));
         }
-
-        combinations = helper(n);
         
-        vector<string> stringVector;
-        for (const auto& binary : combinations) {
-            stringVector.push_back(binary.to_string().substr(32 - n));
-        }
-        string res="";
-        for(int i = 0 ; i < stringVector.size() ; i++){
-            if(ans.find(stringVector[i]) == ans.end()){
-               res = stringVector[i];
-               break;
+        int n = nums.size();
+        for (int num = 0; num <= n; num++) {
+            if (integers.find(num) == integers.end()) {
+                string ans = bitset<16>(num).to_string();
+                return ans.substr(16 - n);
             }
         }
-    return res;
+        
+        return "";
     }
 };
