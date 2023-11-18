628289a863dd703be4e2136578715002f7b7ea42
Shantanu Singh Verma <90418655+Shantanu2003@users.noreply.github.com>
2023-11-19 04:42:08 +0530

    Time: 5 ms (11.00%) | Memory: 6.4 MB (16.39%) - LeetSync

--- 263-ugly-number/ugly-number.cpp
+++ 263-ugly-number/ugly-number.cpp
@@ -1,34 +1,15 @@
 class Solution {
 public:
-    bool isUgly(int n) {
-    vector<int> factors;
-
-    if(n <= 0)
-    return false;
-    // Divide by 2 while n is even
-    while (n % 2 == 0) {
-        factors.push_back(2);
-        n /= 2;
-    }
-
-    // Try odd numbers starting from 3
-    for (long long i = 3; i * i <= n; i += 2) {
-        while (n % i == 0) {
-            factors.push_back(i);
-            n /= i;
+    bool isUgly(int n) 
+    {
+        if (n <= 0) return false;
+        while(n > 1)
+        {
+            if (n % 2 == 0) n = n / 2;
+            else if (n % 3 == 0) n = n / 3;
+            else if (n % 5 == 0) n = n / 5;
+            else break;
         }
-    }
-
-    // If n is a prime greater than 2
-    if (n > 2) {
-        factors.push_back(n);
-    }
-    for(int factor: factors){
-        if(factor !=2 && factor != 3 && factor != 5)
-        return false;
-    }
-
-
-    return true;
+        return (n == 1);
     }
 };
