5a53561c4fb4daf70f41d3e184c39c8189007df5
Shantanu Singh Verma <90418655+Shantanu2003@users.noreply.github.com>
2023-11-20 20:26:09 +0530

    Time: 174 ms (43.75%) | Memory: 102.6 MB (19.78%) - LeetSync

---
+++ 2471-minimum-amount-of-time-to-collect-garbage/minimum-amount-of-time-to-collect-garbage.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int garbageCollection(vector<string>& garbage, vector<int>& travel) {
+        int n = garbage.size();
+        int totalSum = 0;
+        int maxG = 0,maxM = 0,maxP = 0;
+        int g =0 ,m =0, p = 0;
+        for(int s = 0; s<garbage.size(); s++){
+            for(int i =0 ; i<garbage[s].size() ; i++){
+               if(garbage[s][i] == 'G'){
+                   g++;
+                   maxG= max(maxG,s);
+               }
+               else if(garbage[s][i] == 'P'){
+                   p++;
+                   maxP= max(maxP,s);
+               }
+               else{
+                   m++;
+                   maxM= max(maxM,s);
+               }
+            }
+        }
+        for(int i = 0 ; i<= maxG-1; i++){
+        totalSum += travel[i]; 
+        }
+        for(int i = 0 ; i<= maxP-1; i++){
+        totalSum += travel[i]; 
+        }
+        for(int i = 0 ; i<= maxM-1; i++){
+        totalSum += travel[i]; 
+        }
+        totalSum += m+g+p;
+
+        return totalSum;
+    }
+};
