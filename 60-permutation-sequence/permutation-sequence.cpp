13494d35e1752041b4d697ee240c41151408f1a8
Shantanu Singh Verma <90418655+Shantanu2003@users.noreply.github.com>
2023-11-24 22:23:48 +0530

    Time: 3 ms (57.77%) | Memory: 6.7 MB (9.99%) - LeetSync

--- 60-permutation-sequence/permutation-sequence.cpp
+++ 60-permutation-sequence/permutation-sequence.cpp
@@ -2,16 +2,30 @@ class Solution {
 private:
 public:
     string getPermutation(int n, int k) {
-        string res="";
-        for(int  i = 1; i <= n; i++)
-        res += to_string(i);
+        string result;
+        vector<int> nums;
 
-        for (int i = 1; i < k; ++i) {
-        if (!next_permutation(res.begin(), res.end())) {
-            // If there are fewer than k permutations, return an empty string
-            return "";
+        // Initialize nums with numbers from 1 to n
+        for (int i = 1; i <= n; ++i) {
+            nums.push_back(i);
         }
-    }
-    return res;
+
+        // Factorial array to compute factorials quickly
+        vector<int> factorial(n + 1, 1);
+        for (int i = 1; i <= n; ++i) {
+            factorial[i] = i * factorial[i - 1];
+        }
+
+        // Adjust k to 0-based index
+        --k;
+
+        for (int i = n; i > 0; --i) {
+            int index = k / factorial[i - 1];
+            k %= factorial[i - 1];
+            result += to_string(nums[index]);
+            nums.erase(nums.begin() + index);
+        }
+
+        return result;
     }
 };
