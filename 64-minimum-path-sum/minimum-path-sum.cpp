d8fc91c833ef6ab8f3ad7fa627124c18f2ab8d33
Shantanu Singh Verma <90418655+Shantanu2003@users.noreply.github.com>
2023-11-25 03:02:24 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (97.87%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int minPathSum(vector<vector<int>>& grid) {
-                int row = grid.size();
+        int row = grid.size();
         int col = grid[0].size();
 
         // Initialize the first row
