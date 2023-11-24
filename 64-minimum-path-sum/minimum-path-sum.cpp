4953a2cbaaccb13bbb62d41800da1503a36600a9
Shantanu Singh Verma <90418655+Shantanu2003@users.noreply.github.com>
2023-11-25 03:02:20 +0530

    Time: 10 ms (29.94%) | Memory: 10 MB (83.73%) - LeetSync

---
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+                int row = grid.size();
+        int col = grid[0].size();
+
+        // Initialize the first row
+        for (int j = 1; j < col; ++j) {
+            grid[0][j] += grid[0][j - 1];
+        }
+
+        // Initialize the first column
+        for (int i = 1; i < row; ++i) {
+            grid[i][0] += grid[i - 1][0];
+        }
+
+        // Fill in the rest of the grid
+         for (int i = 1; i < row; ++i) {
+            for (int j = 1; j < col; ++j) {
+                grid[i][j] += std::min(grid[i - 1][j], grid[i][j - 1]);
+            }
+        }
+
+        // The bottom-right cell now contains the minimum path sum
+        return grid[row - 1][col - 1];
+    }
+};
