b23c058156e721234030e6555a980155acc34dc6
Shantanu Singh Verma <90418655+Shantanu2003@users.noreply.github.com>
2023-11-30 03:13:37 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (67.87%) - LeetSync

---
+++ 51-n-queens/n-queens.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    void solveNQueensUtil(vector<vector<string>>& result, vector<string>& board, int row, int n) {
+        if (row == n) {
+            result.push_back(board);
+            return;
+        }
+
+        for (int col = 0; col < n; ++col) {
+            if (isValid(board, row, col, n)) {
+                board[row][col] = 'Q';
+                solveNQueensUtil(result, board, row + 1, n);
+                board[row][col] = '.';  // Backtrack
+            }
+        }
+    }
+
+    bool isValid(const vector<string>& board, int row, int col, int n) {
+        // Check if there is a queen in the same column
+        for (int i = 0; i < row; ++i) {
+            if (board[i][col] == 'Q') {
+                return false;
+            }
+        }
+
+        // Check if there is a queen in the left diagonal
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; --i, --j) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+
+        // Check if there is a queen in the right diagonal
+        for (int i = row - 1, j = col + 1; i >= 0 && j < n; --i, ++j) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+
+        return true;
+    }
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string>> result;
+        vector<string> board(n, string(n, '.'));
+
+        solveNQueensUtil(result, board, 0, n);
+        
+        return result;
+    }
+};
