879d76074d2c338737fcbe4310f352de2f7088ca
Shantanu Singh Verma <90418655+Shantanu2003@users.noreply.github.com>
2023-11-30 01:58:40 +0530

    Time: 823 ms (29.35%) | Memory: 8.4 MB (42.25%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -2,30 +2,25 @@ class Solution {
 public:
     bool helper(vector<vector<char>>& board, string word, int row, int col, int index){
         if (index == word.size()) {
+            return true;  // The entire word is found
+        }
+
+        if (row < 0 || row >= board.size() || col < 0 || col >= board[0].size() || board[row][col] != word[index]) {
+            return false;  // Out of bounds or mismatched character
+        }
+
+        char temp = board[row][col];
+        board[row][col] = ' ';  // Mark the cell as visited
+
+        // Explore adjacent cells
+        if (helper(board, word, row + 1, col, index + 1) ||
+            helper(board, word, row - 1, col, index + 1) ||
+            helper(board, word, row, col + 1, index + 1) ||
+            helper(board, word, row, col - 1, index + 1)) {
             return true;
         }
 
-        // Check if the current indices are within bounds
-        if (row >= 0 && row < board.size() && col >= 0 && col < board[0].size()) {
-            // Check if the current character matches the next character in the word
-            if (board[row][col] == word[index]) {
-                // Temporarily mark the current cell as visited
-                char temp = board[row][col];
-                board[row][col] = ' ';
-
-                // Recursively check adjacent cells
-                if (helper(board, word, row + 1, col, index + 1) ||
-                    helper(board, word, row - 1, col, index + 1) ||
-                    helper(board, word, row, col + 1, index + 1) ||
-                    helper(board, word, row, col - 1, index + 1)) {
-                    return true;
-                }
-
-                // Backtrack: restore the original value
-                board[row][col] = temp;
-            }
-        }
-
+        board[row][col] = temp;  // Backtrack: restore the original value
         return false;
     }
     bool exist(vector<vector<char>>& board, string word) {
