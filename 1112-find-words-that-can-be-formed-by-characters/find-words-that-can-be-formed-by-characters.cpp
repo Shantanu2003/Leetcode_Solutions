fb0d9ee5130625721a4c7b4df7664bdd4897f6ba
Shantanu Singh Verma <90418655+Shantanu2003@users.noreply.github.com>
2023-12-02 20:57:05 +0530

    Time: 169 ms (18.84%) | Memory: 50.2 MB (27.72%) - LeetSync

---
+++ 1112-find-words-that-can-be-formed-by-characters/find-words-that-can-be-formed-by-characters.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    bool isFind(const string& s, const unordered_map<char, int>& mp) {
+        unordered_map<char, int> mp1;
+        for (char c : s) {
+            mp1[c]++;
+        }
+
+        for (const auto& kv : mp1) {
+            // Check if the key exists in the map before using at()
+            if (mp.find(kv.first) == mp.end() || kv.second > mp.at(kv.first)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    int countCharacters(vector<string>& words, const string& chars) {
+        unordered_map<char, int> mp;
+        int ans = 0;
+
+        for (char c : chars) {
+            mp[c]++;
+        }
+
+        for (const string& s : words) {
+            if (isFind(s, mp)) {
+                unordered_map<char, int> mp1;
+                for (char c : s) {
+                    mp1[c]++;
+                }
+
+                for (const auto& kv : mp1) {
+                    // Check if the key exists in the map before using at()
+                    if (mp.find(kv.first) != mp.end()) {
+                        ans += min(kv.second, mp.at(kv.first));
+                    }
+                }
+            }
+        }
+
+        return ans;
+    }
+};
