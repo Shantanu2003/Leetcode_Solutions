fb8ab0d2780d7fe3a3849b53eb52d19750d6cee9
Shantanu Singh Verma <90418655+Shantanu2003@users.noreply.github.com>
2023-12-05 03:00:07 +0530

    Time: 21 ms (32.63%) | Memory: 15.2 MB (27.11%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.cpp
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -1,46 +1,46 @@
 class Solution {
 public:
-bool dfs(int node, vector<vector<int>>& adj, vector<bool>& visit, vector<bool>& inStack, vector<int>& res) {
-        if (inStack[node]) {
+bool dfs(int node, vector<vector<int>>& adj, vector<int>& state, vector<int>& res) {
+        if (state[node] == 2) {
+            // Already visited
+            return true;
+        }
+        if (state[node] == 1) {
             // Cycle detected
             return false;
         }
-        if (visit[node]) {
-            return true;
-        }
 
-        visit[node] = true;
-        inStack[node] = true;
+        state[node] = 1; // Mark as visiting
 
         for (auto neighbor : adj[node]) {
-            if (!dfs(neighbor, adj, visit, inStack, res)) {
+            if (!dfs(neighbor, adj, state, res)) {
                 // Propagate the cycle detection
                 return false;
             }
         }
 
-        inStack[node] = false;
+        state[node] = 2; // Mark as visited
         res.push_back(node);
         return true;
     }
 
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
         vector<vector<int>> adj(numCourses);
-        vector<int>res;
+        vector<int> res;
+        vector<int> state(numCourses, 0); // 0: Not visited, 1: Visiting, 2: Visited
+
         for (auto prerequisite : prerequisites) {
             adj[prerequisite[1]].push_back(prerequisite[0]);
         }
 
-        vector<bool> visit(numCourses);
-        vector<bool> inStack(numCourses);
         for (int i = 0; i < numCourses; i++) {
-            if (!visit[i] && !dfs(i, adj, visit, inStack, res)) {
+            if (state[i] == 0 && !dfs(i, adj, state, res)) {
                 // Cycle detected, return an empty array
                 return {};
             }
         }
 
-        reverse(res.begin(),res.end());
+        reverse(res.begin(), res.end()); // Reverse during construction
         return res;
     }
 };
