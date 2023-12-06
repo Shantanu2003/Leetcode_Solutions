adb87ef902e8345fc5377a6d72f3606b616e9daa
Shantanu Singh Verma <90418655+Shantanu2003@users.noreply.github.com>
2023-12-06 21:47:07 +0530

    Time: 2 ms (49.74%) | Memory: 6.6 MB (6.05%) - LeetSync

---
+++ 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int totalMoney(int n) {
+        int res = 1;
+        int ans = 0;
+        for(int  i =0; i < n; i++){
+            if(i % 7 != 0){
+              res += ans+1;
+              ans++;
+            }
+            else{
+                ans = i/7 + 1;
+                res += ans;
+            }
+        }
+        return res-1;
+    }
+};
