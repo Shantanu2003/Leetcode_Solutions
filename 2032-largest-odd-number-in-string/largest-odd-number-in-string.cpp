4843a669571d1a4dbc76a16769356e3ba33e772c
Shantanu Singh Verma <90418655+Shantanu2003@users.noreply.github.com>
2023-12-08 01:53:55 +0530

    Time: 26 ms (82.73%) | Memory: 15.5 MB (24.05%) - LeetSync

--- 2032-largest-odd-number-in-string/largest-odd-number-in-string.cpp
+++ 2032-largest-odd-number-in-string/largest-odd-number-in-string.cpp
@@ -6,13 +6,9 @@ public:
         for(int i = num.size() - 1; i >= 0 ; i--){
             char c = num[i];
             if((c -'0') % 2 != 0){
-                index = i+1;
-                break;
+                return num.substr(0,i+1);
             }
         }
-        for(int i = 0; i < index; i++){
-          res += num[i];
-        }
-    return res;
+    return "";
     }
 };
