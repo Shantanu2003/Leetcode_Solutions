2d8bc2caf386efe959ad0b32060aab3a222e8837
Shantanu Singh Verma <90418655+Shantanu2003@users.noreply.github.com>
2023-12-21 18:36:48 +0530

    Time: 257 ms (24.68%) | Memory: 66.5 MB (87.58%) - LeetSync

--- 1742-widest-vertical-area-between-two-points-containing-no-points/widest-vertical-area-between-two-points-containing-no-points.cpp
+++ 1742-widest-vertical-area-between-two-points-containing-no-points/widest-vertical-area-between-two-points-containing-no-points.cpp
@@ -1,17 +1,12 @@
 class Solution {
 public:
     int maxWidthOfVerticalArea(vector<vector<int>>& points) {
-        priority_queue<int> pq;
+        sort(points.begin(),points.end());
         int res = -1;
-        for(int i = 0; i < points.size();i++){
-          pq.push(points[i][0]);
+        for(int i = 1; i < points.size();i++){
+           res = max(res, points[i][0] - points[i - 1][0]);
         }
-        while(!pq.empty()){
-            int top1 = pq.top();
-            pq.pop();
-            int top2 = pq.top();
-            res = max(res,(top1 - top2));
-        }
+        
         return res;
     }
 };
