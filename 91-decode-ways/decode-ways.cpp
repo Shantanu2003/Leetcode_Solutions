f22d72ba63871c05e57924c2f807d5ba2f7b1630
Shantanu Singh Verma <90418655+Shantanu2003@users.noreply.github.com>
2023-12-25 20:47:36 +0530

    Time: 3 ms (47.64%) | Memory: 6.5 MB (87.76%) - LeetSync

---
+++ 91-decode-ways/decode-ways.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int numDecodings(string s) {
+        int n = s.size();
+        vector<int> count1(n + 1,0);
+        count1[0] = 1;
+
+        for (int i = 1; i <= n; i++) {
+            if (s[i - 1] != '0') {
+                count1[i] += count1[i - 1] ;
+            }
+
+            if (i > 1 && (s[i - 2] == '1' || (s[i - 2] == '2' && s[i - 1] <= '6'))) {
+                count1[i] += count1[i - 2];
+            }
+        }
+
+        return count1[n];
+    }
+};
+
+
