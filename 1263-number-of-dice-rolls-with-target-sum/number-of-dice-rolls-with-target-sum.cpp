8572e45f4428814ee216defe77e993d9515e1ba1
Shantanu Singh Verma <90418655+Shantanu2003@users.noreply.github.com>
2023-12-26 15:52:59 +0530

    Time: 83 ms (5.29%) | Memory: 9 MB (29.50%) - LeetSync

--- 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.cpp
+++ 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.cpp
@@ -2,22 +2,22 @@ class Solution {
 public:
 const int MOD = 1e9 + 7;
     int numRollsToTarget(int n, int k, int target) {
-    vector<int> dp(target + 1, 0);
-    dp[0] = 1;
+    std::vector<std::vector<int>> dp(n + 1, std::vector<int>(target + 1, 0));
 
+    // Base case: There is one way to achieve a sum of 0 with 0 dice.
+    dp[0][0] = 1;
+
+    // Dynamic programming to calculate the number of ways to get each sum with the given number of dice.
     for (int i = 1; i <= n; ++i) {
-        std::vector<int> newDp(target + 1, 0);
         for (int j = 1; j <= target; ++j) {
             for (int face = 1; face <= k; ++face) {
                 if (j - face >= 0) {
-                    newDp[j] = (newDp[j] + dp[j - face]) % MOD;
+                    dp[i][j] = (dp[i][j] + dp[i - 1][j - face]) % MOD;
                 }
             }
         }
-        dp = newDp;
     }
 
-    return dp[target];        
-
+    return dp[n][target];
     }
 };
