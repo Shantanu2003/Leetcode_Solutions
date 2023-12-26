a720408036ee55d0c8ece93167a99610ceee407f
Shantanu Singh Verma <90418655+Shantanu2003@users.noreply.github.com>
2023-12-26 10:51:14 +0530

    Time: 83 ms (70.76%) | Memory: 8.4 MB (86.13%) - LeetSync

---
+++ 375-guess-number-higher-or-lower-ii/guess-number-higher-or-lower-ii.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int getMoneyAmount(int n) {
+       vector<vector<int>> dp(n+1,vector<int>(n+1, 0));
+       for(int  i = 2 ; i<=n; i++){
+           for(int  j =  1 ; j <= n- i+ 1; j++){
+            int mini = INT_MAX;
+            for(int  k = j; k < j+i -1; k++){
+               int res= k+ max(dp[j][k-1], dp[k+1][j+i-1]);
+               mini = min(res,mini);
+            }
+            dp[j][j+i-1] = mini;
+           }
+       }
+       return dp[1][n];
+    }
+};
