53389b451987aa6c21c715500079fdc71fd8fe3e
Shantanu Singh Verma <90418655+Shantanu2003@users.noreply.github.com>
2023-12-26 15:55:06 +0530

    Time: 27 ms (47.77%) | Memory: 9 MB (35.37%) - LeetSync

--- 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.cpp
+++ 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.cpp
@@ -1,23 +1,31 @@
 class Solution {
 public:
-const int MOD = 1e9 + 7;
+    const int mod = (int)pow(10, 9) + 7; // Modulo value for calculations
+
+    // Function to calculate the number of ways to obtain a target sum using 'n' dice rolls with 'k' faces on each die
     int numRollsToTarget(int n, int k, int target) {
-    std::vector<std::vector<int>> dp(n + 1, std::vector<int>(target + 1, 0));
-
-    // Base case: There is one way to achieve a sum of 0 with 0 dice.
-    dp[0][0] = 1;
-
-    // Dynamic programming to calculate the number of ways to get each sum with the given number of dice.
-    for (int i = 1; i <= n; ++i) {
-        for (int j = 1; j <= target; ++j) {
-            for (int face = 1; face <= k; ++face) {
-                if (j - face >= 0) {
-                    dp[i][j] = (dp[i][j] + dp[i - 1][j - face]) % MOD;
-                }
-            }
-        }
+        vector<vector<int>> dp(n + 1, vector<int>(target + 1, -1)); // Create a 2D vector for memoization
+        return recursion(dp, n, k, target); // Call the recursive function with memoization
     }
 
-    return dp[n][target];
+private:
+    // Recursive function to calculate the number of ways to obtain the target sum
+    int recursion(vector<vector<int>>& dp, int n, int k, int target) {
+        // Base cases: if the target sum is reached with 'n' dice rolls or if 'n' becomes 0 or target reaches 0, return accordingly
+        if (target == 0 && n == 0) return 1;
+        if (n == 0 || target <= 0) return 0;
+
+        // If the result is already computed, return the stored value from the memoization table
+        if (dp[n][target] != -1) return dp[n][target] % mod;
+
+        int ways = 0; // Variable to count the number of ways to obtain the target sum
+        for (int i = 1; i <= k; i++) {
+            // Calculate the number of ways by considering each possible face value of the die
+            ways = (ways + recursion(dp, n - 1, k, target - i)) % mod;
+        }
+        dp[n][target] = ways % mod; // Store the computed result in the memoization table
+        return dp[n][target];
     }
 };
+
+
