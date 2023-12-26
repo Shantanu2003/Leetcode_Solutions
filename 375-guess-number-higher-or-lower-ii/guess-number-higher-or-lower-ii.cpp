2cc3ff24338ad16c8bc4f04d188e7de3d0380d47
Shantanu Singh Verma <90418655+Shantanu2003@users.noreply.github.com>
2023-12-26 10:52:28 +0530

    Time: 37 ms (97.31%) | Memory: 8.8 MB (13.87%) - LeetSync

--- 375-guess-number-higher-or-lower-ii/guess-number-higher-or-lower-ii.cpp
+++ 375-guess-number-higher-or-lower-ii/guess-number-higher-or-lower-ii.cpp
@@ -5,7 +5,7 @@ public:
        for(int  i = 2 ; i<=n; i++){
            for(int  j =  1 ; j <= n- i+ 1; j++){
             int mini = INT_MAX;
-            for(int  k = j; k < j+i -1; k++){
+            for(int  k = j+(i-1)/2; k < j+i -1; k++){
                int res= k+ max(dp[j][k-1], dp[k+1][j+i-1]);
                mini = min(res,mini);
             }
