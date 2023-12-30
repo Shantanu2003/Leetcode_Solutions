3a52fce07c8c1b9ad4125a4d1ae770b64edbe3a5
Shantanu Singh Verma <90418655+Shantanu2003@users.noreply.github.com>
2023-12-30 23:15:00 +0530

    Time: 15 ms (52.92%) | Memory: 12.3 MB (49.42%) - LeetSync

--- 2025-redistribute-characters-to-make-all-strings-equal/redistribute-characters-to-make-all-strings-equal.cpp
+++ 2025-redistribute-characters-to-make-all-strings-equal/redistribute-characters-to-make-all-strings-equal.cpp
@@ -4,6 +4,16 @@ public:
         vector<int>ans(26,0);
         if(words.size() == 1)
         return true; 
+        
+        int totalCharCount = 0;
+        for (const string& s : words) {
+            totalCharCount += s.length();
+        }
+
+        if (totalCharCount % words.size() != 0) {
+            return false;
+        }
+
         for(string word: words){
             for(char c :word){
                 ans[c-'a']++;;
