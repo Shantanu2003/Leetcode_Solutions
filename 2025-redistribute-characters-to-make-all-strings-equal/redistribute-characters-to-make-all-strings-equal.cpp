0f2b9306a9ad9750ba7dbe779156b72adad5d0d2
Shantanu Singh Verma <90418655+Shantanu2003@users.noreply.github.com>
2023-12-30 23:06:34 +0530

    Time: 10 ms (85.99%) | Memory: 12.5 MB (44.75%) - LeetSync

--- 2025-redistribute-characters-to-make-all-strings-equal/redistribute-characters-to-make-all-strings-equal.cpp
+++ 2025-redistribute-characters-to-make-all-strings-equal/redistribute-characters-to-make-all-strings-equal.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     bool makeEqual(vector<string>& words) {
-        unordered_map<char,int>mp;
+        vector<int>ans(26,0);
         if(words.size() == 1)
         return true; 
         for(string word: words){
             for(char c :word){
-                mp[c]++;
+                ans[c-'a']++;;
             }
         }
 
-        for(auto i:mp){
-            if(i.second % words.size() != 0)
+        for(auto i:ans){
+            if(i % words.size() != 0)
             return false;
         }
         return true;
