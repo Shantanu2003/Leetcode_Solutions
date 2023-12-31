ec63e95b553694b909da6a7f7c7d982abdc89d0b
Shantanu Singh Verma <90418655+Shantanu2003@users.noreply.github.com>
2023-12-31 08:43:05 +0530

    Time: 0 ms (100.00%) | Memory: 7.2 MB (27.55%) - LeetSync

--- 93-restore-ip-addresses/restore-ip-addresses.cpp
+++ 93-restore-ip-addresses/restore-ip-addresses.cpp
@@ -1,25 +1,47 @@
 class Solution {
-public:
-    vector<string> restoreIpAddresses(string s) {
-        int len = s.length();
-        vector<string> result;
-        dfs(result, s, 0,"");
-        return result;
-
-    }
-    void dfs(vector<string> &result, string &s, int start, string current) {
-    if (start == s.length() && count(current.begin(), current.end(), '.') == 3) {
-        result.push_back(current);
-        return;
+    bool valid(const string& s, int start, int length) {
+        return length == 1 || (s[start] != '0' && (length < 3 || s.substr(start, length) <= "255"));
     }
 
-    for (int i = 1; i <= 3; i++) {
-        if (start + i <= s.length()) {
-            string segment = s.substr(start, i);
-            if ((segment[0] != '0' || segment.size() == 1) && stoi(segment) <= 255) {
-                dfs(result, s, start + i, current + segment + (start + i == s.length() ? "" : "."));
+    void helper(const string& s, int startIndex, vector<int>& dots, vector<string>& ans) {
+        const int remainingLength = s.length() - startIndex;
+        const int remainingNumberOfIntegers = 4 - dots.size();
+
+        if (remainingLength > remainingNumberOfIntegers * 3 ||
+            remainingLength < remainingNumberOfIntegers) {
+            return;
+        }
+        if (dots.size() == 3) {
+            if (valid(s, startIndex, remainingLength)) {
+                string temp;
+                int last = 0;
+                for (int dot : dots) {
+                    temp.append(s.substr(last, dot));
+                    last += dot;
+                    temp.append(".");
+                }
+                temp.append(s.substr(startIndex));
+                ans.push_back(temp);
             }
+            return;
+        }
+        for (int curPos = 1; curPos <= 3 && curPos <= remainingLength; ++curPos) {
+            // Append a dot at the current position.
+            dots.push_back(curPos);
+            // Try making all combinations with the remaining string.
+            if (valid(s, startIndex, curPos)) {
+                helper(s, startIndex + curPos, dots, ans);
+            }
+            // Backtrack, i.e. remove the dot to try placing it at the next position.
+            dots.pop_back();
         }
     }
-}
+
+public:
+    vector<string> restoreIpAddresses(string s) {
+        vector<int> dots;
+        vector<string> ans;
+        helper(s, 0, dots, ans);
+        return ans;
+    }
 };
