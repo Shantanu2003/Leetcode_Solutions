ef961f4cd50d300110e880e9991bbd10257029a2
Shantanu Singh Verma <90418655+Shantanu2003@users.noreply.github.com>
2024-01-03 08:34:11 +0530

    Time: 9 ms (72.45%) | Memory: 30.1 MB (62.38%) - LeetSync

--- 2724-convert-an-array-into-a-2d-array-with-conditions/convert-an-array-into-a-2d-array-with-conditions.cpp
+++ 2724-convert-an-array-into-a-2d-array-with-conditions/convert-an-array-into-a-2d-array-with-conditions.cpp
@@ -1,19 +1,16 @@
 class Solution {
 public:
     vector<vector<int>> findMatrix(vector<int>& nums) {
-        vector<int> freq(nums.size() + 1);
-        
-        vector<vector<int>> ans;
-        for (int c : nums) {
-            if (freq[c] >= ans.size()) {
-                ans.push_back({});
-            }
-            
-            // Store the integer in the list corresponding to its current frequency.
-            ans[freq[c]].push_back(c);
-            freq[c]++;
+        vector<vector<int>>ans;
+        unordered_map<int,int> rowFrequency;
+
+        for(int i= 0; i < nums.size() ; i++){
+          if(rowFrequency[nums[i]] >= ans.size()){
+              ans.push_back({});
+          }
+           ans[rowFrequency[nums[i]]].push_back(nums[i]);
+           rowFrequency[nums[i]]++;
         }
-        
         return ans;
     }
 };
