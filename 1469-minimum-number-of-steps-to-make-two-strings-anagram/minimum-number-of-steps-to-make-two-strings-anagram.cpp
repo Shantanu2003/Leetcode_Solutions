642dd541e3cdad24f20617a0decee898fb466340
Shantanu Singh Verma <90418655+Shantanu2003@users.noreply.github.com>
2024-01-14 00:45:25 +0530

    Time: 35 ms (98.18%) | Memory: 16.9 MB (62.45%) - LeetSync

--- 1469-minimum-number-of-steps-to-make-two-strings-anagram/minimum-number-of-steps-to-make-two-strings-anagram.cpp
+++ 1469-minimum-number-of-steps-to-make-two-strings-anagram/minimum-number-of-steps-to-make-two-strings-anagram.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
     int minSteps(string s, string t) {
-        unordered_map<int,int>forS;
-        unordered_map<int,int>forT;
-        int res = 0;
-        for(int  i = 0 ; i < s.size(); i++){
-            forS[s[i]]++;
-            forT[t[i]]++;
-        }
-        for(auto i :forT){
-            if(i.second > forS[i.first])
-            res += i.second-forS[i.first];
+        int count[26] = {0};
+        // Storing the difference of frequencies of characters in t and s.
+        for (int i = 0; i < s.size(); i++) {
+            count[t[i] - 'a']++;
+            count[s[i] - 'a']--;
         }
 
-    return res;
+        int ans = 0;
+        // Adding the difference where string t has more instances than s.
+        // Ignoring where t has fewer instances as they are redundant and
+        // can be covered by the first case.
+        for (int i = 0; i < 26; i++) {
+            ans += max(0, count[i]);
+        }
+        
+        return ans;
     }
 };
