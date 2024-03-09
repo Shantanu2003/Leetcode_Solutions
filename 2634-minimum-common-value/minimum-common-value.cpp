f1b9a54560595cfd233b4b7b8f1ef855c0b69934
Shantanu Singh Verma <90418655+Shantanu2003@users.noreply.github.com>
2024-03-10 03:53:26 +0530

    Time: 81 ms (30.08%) | Memory: 52.9 MB (51.69%) - LeetSync

--- 2634-minimum-common-value/minimum-common-value.cpp
+++ 2634-minimum-common-value/minimum-common-value.cpp
@@ -1,14 +1,38 @@
 class Solution {
 public:
     int getCommon(vector<int>& nums1, vector<int>& nums2) {
-       set<int>s;
-       for(int  i : nums1)
-       s.insert(i);
+        // Binary search should be done on the larger array
+        // If nums1 is longer, call getCommon with the arrays swapped
+        if (nums1.size() > nums2.size()) {
+            return getCommon(nums2, nums1);
+        }
 
-       for(int num: nums2){
-           if(s.find(num) != s.end())
-           return num;
-       }
-       return -1;
+        // Search for each element of nums1 in nums2
+        // Return the first common element found
+        for (int num : nums1) {
+            if (binarySearch(num, nums2)) {
+                return num;
+            }
+        }
+
+        // Return -1 if there are no common elements
+        return -1;
+    }
+
+private:
+    bool binarySearch(int target, vector<int>& nums) {
+        int left = 0;
+        int right = nums.size() - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] > target) {
+                right = mid - 1;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                return true;
+            }
+        }
+        return false;
     }
 };
