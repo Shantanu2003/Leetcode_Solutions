dad730bfbb24d9f929dccc9107c0e31b1bcd8f0e
Shantanu Singh Verma <90418655+Shantanu2003@users.noreply.github.com>
2024-05-08 23:55:06 +0530

    Time: 6 ms (81.94%) | Memory: 13.5 MB (89.04%) - LeetSync

---
+++ 506-relative-ranks/relative-ranks.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<string> findRelativeRanks(vector<int>& score) {
+        priority_queue<pair<int,int>>pq;
+        for(int  i = 0 ; i < score.size() ; i++){
+            pq.push({score[i], i});
+        }
+
+        vector<string>res(score.size());
+        auto top1 = pq.top();
+        res[top1.second] = "Gold Medal";
+        pq.pop();
+
+        if(!pq.empty()){
+        auto top2 = pq.top();
+        res[top2.second] = "Silver Medal";
+        pq.pop();  
+        }
+        if(!pq.empty()){
+        auto top3 = pq.top();
+        res[top3.second] = "Bronze Medal";
+        pq.pop();
+        }
+                
+        int counter = 4;
+        while(!pq.empty() && counter <= score.size()){
+            auto front = pq.top();
+            pq.pop();
+            res[front.second] = to_string(counter);
+            counter++;
+        }
+        return res;
+    }
+};
