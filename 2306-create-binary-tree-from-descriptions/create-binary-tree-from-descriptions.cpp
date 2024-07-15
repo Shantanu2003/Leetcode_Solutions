466782de303dc447d8da2be59254b6199fe7e105
Shantanu Singh Verma <90418655+Shantanu2003@users.noreply.github.com>
2024-07-16 03:59:10 +0530

    Time: 978 ms (7.33%) | Memory: 448.3 MB (5.17%) - LeetSync

--- 2306-create-binary-tree-from-descriptions/create-binary-tree-from-descriptions.cpp
+++ 2306-create-binary-tree-from-descriptions/create-binary-tree-from-descriptions.cpp
@@ -11,67 +11,43 @@
  */
 class Solution {
 public:
-    TreeNode* insertLevelOrder(const vector<string>& arr, int i) {
-        TreeNode* root = nullptr;
-        if (i < arr.size() && arr[i] != "null") {
-            root = new TreeNode(stoi(arr[i]));
-            root->left = insertLevelOrder(arr, 2 * i + 1);
-            root->right = insertLevelOrder(arr, 2 * i + 2);
-        }
-        return root;
-    }
-
     TreeNode* createBinaryTree(vector<vector<int>>& descriptions) {
-        unordered_map<int, TreeNode*> nodes;
-        unordered_map<int, int> leftChild;
-        unordered_map<int, int> rightChild;
-        unordered_set<int> children;
-
-        for (const auto& desc : descriptions) {
-            int parent = desc[0];
-            int child = desc[1];
-            int isLeft = desc[2];
-
-            if (nodes.find(parent) == nodes.end())
-                nodes[parent] = new TreeNode(parent);
-            if (nodes.find(child) == nodes.end())
-                nodes[child] = new TreeNode(child);
-
-            if (isLeft)
-                leftChild[parent] = child;
-            else
-                rightChild[parent] = child;
-
-            children.insert(child);
+        unordered_map<int,unordered_map<int,int>>mp;
+        unordered_set<int>s;
+        for(auto it: descriptions){
+            mp[it[0]][it[1]] = it[2]; 
+            s.insert(it[1]);
         }
-
-        // Find the root
-        int rootVal = -1;
-        for (const auto& [key, _] : nodes) {
-            if (children.find(key) == children.end()) {
-                rootVal = key;
+        vector<int>order;
+        queue<TreeNode*>q;
+        TreeNode* root = nullptr;
+        for(auto i : mp){
+            if(s.find(i.first) == s.end()){
+                root= new TreeNode(i.first);
+                q.push(root);
                 break;
             }
         }
 
-        TreeNode* root = nodes[rootVal];
-        queue<TreeNode*> q;
-        q.push(root);
-
-        while (!q.empty()) {
-            TreeNode* node = q.front();
+        while(!q.empty()){
+            TreeNode* top = q.front();
             q.pop();
 
-            if (leftChild.find(node->val) != leftChild.end()) {
-                node->left = nodes[leftChild[node->val]];
-                q.push(node->left);
+
+            if(mp.find(top->val) != mp.end()){
+            unordered_map<int,int>child = mp[top->val];
+            for(auto i : child){
+                if(i.second == 1){
+                    top->left = new TreeNode(i.first);
+                    q.push(top->left);
+                }
+                else{
+                    top->right = new TreeNode(i.first);
+                    q.push(top->right);
+                }
             }
-            if (rightChild.find(node->val) != rightChild.end()) {
-                node->right = nodes[rightChild[node->val]];
-                q.push(node->right);
             }
         }
-
-        return root;
+    return root;
     }
 };
