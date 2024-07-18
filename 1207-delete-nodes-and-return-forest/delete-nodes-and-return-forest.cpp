84db2631856bccce34a307a2e3ff134d9853dbf9
Shantanu Singh Verma <90418655+Shantanu2003@users.noreply.github.com>
2024-07-18 15:55:12 +0530

    Time: 14 ms (70.72%) | Memory: 27.2 MB (24.99%) - LeetSync

--- 1207-delete-nodes-and-return-forest/delete-nodes-and-return-forest.cpp
+++ 1207-delete-nodes-and-return-forest/delete-nodes-and-return-forest.cpp
@@ -9,46 +9,39 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
 class Solution {
 public:
     vector<TreeNode*> delNodes(TreeNode* root, vector<int>& to_delete) {
-        unordered_set<int> toDeleteSet(to_delete.begin(), to_delete.end());
         vector<TreeNode*> forest;
 
-        root = processNode(root, toDeleteSet, forest);
+        root = solve(root, to_delete, forest);
 
-        // If the root is not deleted, add it to the forest
-        if (root) {
+        if (root != nullptr) {
             forest.push_back(root);
         }
 
         return forest;
     }
-
-private:
-    TreeNode* processNode(TreeNode* node, unordered_set<int>& toDeleteSet,
-                          vector<TreeNode*>& forest) {
-        if (!node) {
+    TreeNode* solve(TreeNode* root, vector<int>& to_delete, vector<TreeNode*>& forest) {
+        if (root == nullptr) {
             return nullptr;
         }
 
-        node->left = processNode(node->left, toDeleteSet, forest);
-        node->right = processNode(node->right, toDeleteSet, forest);
+        root->left = solve(root->left, to_delete, forest);
+        root->right = solve(root->right, to_delete, forest);
 
-        // Node Evaluation: Check if the current node needs to be deleted
-        if (toDeleteSet.find(node->val) != toDeleteSet.end()) {
-            // If the node has left or right children, add them to the forest
-            if (node->left) {
-                forest.push_back(node->left);
+        if (find(to_delete.begin(), to_delete.end(), root->val) != to_delete.end()) {
+            if (root->left != nullptr) {
+                forest.push_back(root->left);
             }
-            if (node->right) {
-                forest.push_back(node->right);
+            if (root->right != nullptr) {
+                forest.push_back(root->right);
             }
-            // Delete the current node and return null to its parent
-            delete node;
+            delete root;
             return nullptr;
         }
 
-        return node;
+        return root;
     }
 };
