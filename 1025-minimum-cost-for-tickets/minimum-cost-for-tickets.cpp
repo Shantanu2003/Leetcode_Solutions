546bb6b34c0f63ffb08937e6e3dcd2ee7103ca8b
Shantanu Singh Verma <90418655+Shantanu2003@users.noreply.github.com>
2024-07-23 03:12:44 +0530

    Time: 3 ms (58.60%) | Memory: 12 MB (41.48%) - LeetSync

--- 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.cpp
+++ 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.cpp
@@ -1,31 +1,58 @@
 class Solution {
 public:
-    int solve(vector<int>& days, vector<int>& costs, int i, vector<int>& dp){
-        if(i >= days.size())
-        return 0;
+    // int solve(vector<int>& days, vector<int>& costs, int i, vector<int>& dp){
+    //     if(i >= days.size())
+    //     return 0;
 
-        if(dp[i] != -1)
-        return dp[i];
+    //     if(dp[i] != -1)
+    //     return dp[i];
 
-        int res1 = costs[0] + solve(days, costs, i + 1, dp);
+    //     int res1 = costs[0] + solve(days, costs, i + 1, dp);
 
-        int j;
+    //     int j;
 
-        for(j = i; j < days.size() && days[j] < days[i] + 7 ; j++);
+    //     for(j = i; j < days.size() && days[j] < days[i] + 7 ; j++);
 
-        int res2 = costs[1] + solve(days, costs, j,dp);
+    //     int res2 = costs[1] + solve(days, costs, j,dp);
 
-        for(j = i; j < days.size() && days[j] < days[i] + 30 ; j++);
+    //     for(j = i; j < days.size() && days[j] < days[i] + 30 ; j++);
 
-        int res3 = costs[2] + solve(days, costs, j,dp);
+    //     int res3 = costs[2] + solve(days, costs, j,dp);
 
-        dp[i] = min(res1,min(res2,res3));
+    //     dp[i] = min(res1,min(res2,res3));
         
-        return dp[i];
+    //     return dp[i];
+
+    // }
+
+    int solve(vector<int>& days, vector<int>& costs, int i){
+        int n = days.size();
+        vector<int>dp(n+1, INT_MAX);
+        
+        dp[n] = 0;
+
+        for(int  k = n-1 ; k >= 0 ; k--){
+
+            int res1 = costs[0] + dp[k+1];
+
+            int j;
+
+            for(j = k; j < days.size() && days[j] < days[k] + 7 ; j++);
+
+            int res2 = costs[1] + dp[j];
+
+            for(j = k; j < days.size() && days[j] < days[k] + 30 ; j++);
+
+            int res3  = costs[2] + dp[j]; 
+
+            dp[k] = min(res1,min(res2,res3));
+
+        }
+        
+        return dp[0];
 
     }
     int mincostTickets(vector<int>& days, vector<int>& costs) {
-        vector<int>dp(days.size(), -1);
-        return solve(days,costs,0,dp);
+        return solve(days,costs,0);
     }
 };
