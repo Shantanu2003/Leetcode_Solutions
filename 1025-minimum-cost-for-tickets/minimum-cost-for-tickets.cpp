8db25b3bb96707a6e6e76489ff8e8f823f6a05ce
Shantanu Singh Verma <90418655+Shantanu2003@users.noreply.github.com>
2024-07-23 02:24:46 +0530

    Time: 3 ms (58.60%) | Memory: 11.7 MB (71.12%) - LeetSync

---
+++ 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int solve(vector<int>& days, vector<int>& costs, int i, vector<int>& dp){
+        if(i >= days.size())
+        return 0;
+
+        if(dp[i] != -1)
+        return dp[i];
+
+        int res1 = costs[0] + solve(days, costs, i + 1, dp);
+
+        int j;
+
+        for(j = i; j < days.size() && days[j] < days[i] + 7 ; j++);
+
+        int res2 = costs[1] + solve(days, costs, j,dp);
+
+        for(j = i; j < days.size() && days[j] < days[i] + 30 ; j++);
+
+        int res3 = costs[2] + solve(days, costs, j,dp);
+
+        dp[i] = min(res1,min(res2,res3));
+        
+        return dp[i];
+
+    }
+    int mincostTickets(vector<int>& days, vector<int>& costs) {
+        vector<int>dp(days.size(), -1);
+        return solve(days,costs,0,dp);
+    }
+};
