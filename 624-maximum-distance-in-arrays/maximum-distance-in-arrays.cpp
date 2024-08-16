92e78f0b24223fc6d1a9c66eb3b46ab84e178941
Shantanu Singh Verma <90418655+Shantanu2003@users.noreply.github.com>
2024-08-17 04:04:34 +0530

    Time: 230 ms (62.74%) | Memory: 107.8 MB (57.08%) - LeetSync

--- 624-maximum-distance-in-arrays/maximum-distance-in-arrays.cpp
+++ 624-maximum-distance-in-arrays/maximum-distance-in-arrays.cpp
@@ -9,3 +9,30 @@ public:
         return maxDif;
     }
 };
+
+/*
+diff = 0
+currmin = 1
+curr max = 3
+
+diff = 5-1, 3-4 = 4
+currmin = 1
+currmax = 5
+
+diff = 3-1, 5-1 = 4
+currmin = 1
+currmax = 5
+
+{2,3,4}{1,5}{2,3,4}
+
+diff = 4-1000, -1000-1, 0 = 0
+currmin = 2
+currmax = 4
+
+diff = 5-2, 4-1 = 3
+currmin = 1
+currmax = 5
+
+diff = 4-1, 5-2 = 3
+currmin = 1
+currmax = 5 */
