737edec23e7a7059d5fe451b5375e5ba04a4bd16
Shantanu Singh Verma <90418655+Shantanu2003@users.noreply.github.com>
2024-08-19 03:32:39 +0530

    Time: 6 ms (20.88%) | Memory: 10.5 MB (26.76%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -1,13 +1,48 @@
 class Solution {
 public:
-    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-        int m = obstacleGrid.size(), n = obstacleGrid[0].size();
-        vector<vector<int> > dp(m + 1, vector<int> (n + 1, 0));
-        dp[0][1] = 1;
-        for (int i = 1; i <= m; i++)
-            for (int j = 1; j <= n; j++)
-                if (!obstacleGrid[i - 1][j - 1])
-                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
-        return dp[m][n];
-    } 
+vector<int>dr = {-1,0};
+vector<int>dc = {0,-1};
+bool isValid(vector<vector<int>>& grid, int row, int col, int n ,int m){
+    if(row >= 0 && col >= 0 && row < n && col < m && grid[row][col] != 1)
+    return true;
+
+    return false;
+}
+    int helper(vector<vector<int>>& grid, int row, int col, int n , int m, vector<vector<int>>& dp){
+        if (grid[row][col] == 1) {
+            return 0;
+        }
+
+        if(row == 0 && col == 0){
+            return 1;
+        }
+
+        if(row < 0 || col < 0)
+        return 0;
+
+        if(dp[row][col] != -1)
+        return dp[row][col];
+
+        dp[row][col] = 0; 
+        
+        for(int i = 0 ; i < 2 ; i++){
+            int newRow = row + dr[i];
+            int newCol = col + dc[i];
+            
+            if(isValid(grid, newRow, newCol, n ,m)){
+               dp[row][col] += helper(grid, newRow, newCol,  n ,m ,dp);
+            }
+        }
+
+
+        return dp[row][col];
+
+    }
+    int uniquePathsWithObstacles(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<vector<int>> dp(n, vector<int>(m,-1));
+
+        return helper(grid, n-1,m-1, n ,m,dp);
+    }
 };
